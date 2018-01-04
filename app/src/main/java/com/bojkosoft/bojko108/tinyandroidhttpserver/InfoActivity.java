3565ef194f893398ab6c132845f81ee932083b2c
bojko108 <bojko108@gmail.com>
2018-01-04 15:03:26 +0200

    add InfoActivity with information about how to use the application

---
+++ app/src/main/java/com/bojkosoft/bojko108/tinyandroidhttpserver/InfoActivity.java
@@ -0,0 +1,13 @@
+package com.bojkosoft.bojko108.tinyandroidhttpserver;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
